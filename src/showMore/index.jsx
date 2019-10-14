80dc9e9c28983ba2bc38a146237efa66e9bbb830
Christos Paschalidis <paschalidi@outlook.com>
2019-10-14 14:31:07 +0200

    eslint 100 percent

--- src/showMore/index.jsx
+++ src/showMore/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { action } from "../infra/dispatch";
 import { SHOW_MORE_STORIES } from "./action";
 
 const ShowMoreComponent = ({ dispatch, id }) => {
   return (
-    <button onClick={() => dispatch(action(SHOW_MORE_STORIES, id))}>
+    <button
+      type="button"
+      onClick={() => dispatch(action(SHOW_MORE_STORIES, id))}
+    >
       more stories
     </button>
   );
+};
+
+ShowMoreComponent.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  id: PropTypes.oneOf(["topStories", "jobStories", "askStories", "showStories"])
+    .isRequired
 };
 
 export const ShowMore = connect()(ShowMoreComponent);
--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { getStoriesEpic } from "./epic";
 import { ShowMore } from "../showMore";
+import {
+  FETCH_ASK_STORY,
+  FETCH_JOBS_STORY,
+  FETCH_SHOW_STORY,
+  FETCH_TOP_STORY
+} from "./action";
 
 class StoriesComponent extends React.Component {
   componentDidMount() {
@@ -30,8 +37,8 @@ class StoriesComponent extends React.Component {
         <ul>
           {stories[id] &&
             Object.values(stories[id]).map(
-              ({ url, title, by, id, score, time, kids = [] }) => (
-                <li key={id}>
+              ({ url, title, by, id: storyId, score, time, kids = [] }) => (
+                <li key={storyId}>
                   <a href={url}>{title}</a>
                   <div>
                     {score} by {by} {time} ago
@@ -48,6 +55,29 @@ class StoriesComponent extends React.Component {
     );
   }
 }
+StoriesComponent.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  id: PropTypes.oneOf(["topStories", "jobStories", "askStories", "showStories"])
+    .isRequired,
+  actionType: PropTypes.oneOf([
+    FETCH_TOP_STORY,
+    FETCH_ASK_STORY,
+    FETCH_JOBS_STORY,
+    FETCH_SHOW_STORY
+  ]).isRequired,
+  pages: PropTypes.shape({
+    topStories: PropTypes.number.isRequired,
+    showStories: PropTypes.number.isRequired,
+    askStories: PropTypes.number.isRequired,
+    jobStories: PropTypes.number.isRequired
+  }).isRequired,
+  stories: PropTypes.shape({
+    topStories: PropTypes.number.isRequired,
+    showStories: PropTypes.number.isRequired,
+    askStories: PropTypes.number.isRequired,
+    jobStories: PropTypes.number.isRequired
+  }).isRequired
+};
 
 export const Stories = connect(({ storiesState, pageState }) => ({
   ...storiesState,
